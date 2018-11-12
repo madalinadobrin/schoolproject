94230358fca8f2b52e038018eb20765e8d34ade3
madalinadobrin <44975032+madalinadobrin@users.noreply.github.com>
2018-11-12 17:20:59 +0200

    Add files via upload

---
+++ madalinadobrin_2set4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int a,b;
+int main()
+{
+    cin>>a;
+    cin>>b;
+    if(a==b) {cout<<a;}
+    while(a!=b)
+    (if(a>b) {a<--a-b;}
+    else {b<--b-a;}
+    }
+    if(a,b==1)
+    cout<<"DA"
+    cout<<"NU"
+
+    return 0;}
