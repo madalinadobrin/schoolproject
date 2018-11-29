5774af736fa2a186b1d7c926951ae2e0ebe8229a
madalinadobrin <44975032+madalinadobrin@users.noreply.github.com>
2018-11-29 16:49:22 +0200

    Add files via upload

---
+++ madalinadobrin_2set5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int n;
+int main ()
+{
+    cout<<"n=";cin>>n;
+    if(n==1){cout<<0"0";return 0;}
+    while (n>0);
+    n=n>>1;
+    x=x+1;
+    cout<<"x";
+}
